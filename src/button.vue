e5383095c0213d10807c17576c61e8a867ec850a
xiaoxinwan <lixinxi1010@gmail.com>
2018-10-27 20:10:44 +0800

    使用npm test和 npm dev-test 自动åæ试

--- src/button.vue
+++ src/button.vue
@@ -10,8 +10,12 @@
 </template>
 <script>
     // js
+    import Icon from './icon'
+
     export default {
-        // props: ['icon','iconPosition']
+        components:{
+            'g-icon': Icon
+        },
         props: {
             icon: {},
             load:{

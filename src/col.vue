eb5a4db64c4624c9878e1d025dd435c45c435f16
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-01 22:01:29 +0800

    添加row、col组件

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'LunziCol',
+        props: {
+            span: {
+                type : [Number,String]
+            }
+        }
+
+    }
+</script>
+<style scoped lang="scss">
+    .col{
+        height: 100px;
+        background: #666666;
+        width: 50%;
+        border: 2px solid dodgerblue;
+
+        $class-prefix: col-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>

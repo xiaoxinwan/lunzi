5441eee864fd86dc5381d166d93cb3aba3105f5d
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-03 10:12:58 +0800

    默认布局功能基本完成

---
+++ src/content.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="content">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+    .content{
+        flex-grow: 1;
+    }
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'LunziSider',
+
+    }
+</script>
+<style lang="scss" scoped>
+    .sider{
+
+    }
+</style>

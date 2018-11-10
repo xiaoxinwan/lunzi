fc1bad66e57dc64ddd14697e7fcb5e0a9cdc20d9
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-10 14:59:18 +0800

    popover添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -23,8 +23,8 @@
                 this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
             },
             onClickDocument(e) {
-                if (this.$refs.popover &&
-                    (this.$refs.popover === e.target  || this.$refs.popover.contains(e.target))
+                if (this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target  || this.$refs.contentWrapper.contains(e.target))
                 ) {
                    return
                 }else{
@@ -58,15 +58,40 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #666;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
         position: relative;
     }
-
     .content-wrapper {
         position: absolute;
-        border: 1px solid blue;
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        filter: drop-shadow(0 1px 1px rgba(0,0,0,.5));
+        background: #ffffff;
         transform: translateY(-100%);
+        padding: .5em 1em;
+        margin-top: -10px;
+        max-width: 20em;
+        word-break: break-all;
+        &::before, &::after{
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before{
+            border-top-color: $border-color;
+            top: 100%;
+        }
+        &::after{
+            border-top-color: white;
+            top: calc(100% - 2px);
+        }
     }
 </style>

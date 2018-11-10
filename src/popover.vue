c4dd20704e2cd2eeacf1360c568a3aec35f23539
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-10 10:12:37 +0800

    解决容器有overflow:hidden造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -16,13 +18,17 @@
         methods: {
             xxx() {
                 this.visible = !this.visible
-                if(this.visible === true){
-                    this.$nextTick(()=>{
-                      let eventHandler = ()=>{
-                          this.visible = false
-                          document.removeEventListener('click', eventHandler)
-                      }
-                      document.addEventListener('click', eventHandler)
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                        let eventHandler = () => {
+                            this.visible = false
+                            document.removeEventListener('click', eventHandler)
+                        }
+                        document.addEventListener('click', eventHandler)
                     })
                 }
             }
@@ -35,11 +41,11 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper{
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid blue;
-        }
+    }
+
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid blue;
+        transform: translateY(-100%);
     }
 </style>

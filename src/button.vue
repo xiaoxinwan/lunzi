b2b1fb25f4ec22dfe86c20fa207e6a89a69461bf
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-16 14:35:53 +0800

    创建文档官网

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,9 @@
 <template>
     <!--html-->
-    <button id="hhh" class="lxx g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
-        <g-icon  class="icon" v-if="icon && !load" :name="icon"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !load" :name="icon"></g-icon>
         <g-icon class="load icon" v-if="load" name="load"></g-icon>
-        <div class="content">
+        <div class="g-button-content">
             <slot></slot>
         </div>
     </button>
@@ -14,71 +14,73 @@
 
     export default {
         name: 'LunziButton',
-        components:{
+        components: {
             'g-icon': Icon
         },
         props: {
             icon: {},
-            load:{
+            load: {
                 type: Boolean,
                 default: false
             },
             iconPosition: {
                 type: String,
                 default: 'left',
-                validator(value){
-                    return value !== 'left' && value !== 'right' ? false : true;
+                validator(value) {
+                    return value === 'left' || value === 'right'
                 }
-
             }
         }
     }
 </script>
 <style lang="scss" scoped>
     /*css*/
+    $button-height: 32px;
+    $font-size: 14px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
+
     @keyframes spin {
-        0%{transform: rotate(0deg);}
-        100%{transform: rotate(360deg);}
+        0% {transform: rotate(0deg);}
+        100% {transform: rotate(360deg);}
 
     }
-    .g-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
+
+    .g-button {
+        font-size: $font-size;
+        height: $button-height;
         padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+        border-radius: $border-radius;
+        border: 1px solid $border-color;
+        background: $button-bg;
         display: inline-flex;
         justify-content: center;
         align-items: center;
-        vertical-align: top;
-        >.icon {
-            order: 1;
-            margin-right: 5px;
+        vertical-align: middle;
+        &:hover {
+            border-color: $border-color-hover;
         }
-        >.content{
-            order: 2;
+        &:active {
+            background-color: $button-active-bg;
         }
-        &:hover{
-            border-color: var(--border-color-hover);
-        }
-        &:active{
-            background-color: var(--button-active-bg);
-        }
-        &:focus{
+        &:focus {
             outline: none;
         }
-        &.icon-right{
-            >.content{
-                order: 1;
-            }
-            >.icon{
-                order: 2;
-                margin-right: 0;
-                margin-left: 5px;
-            }
+        > .g-button-content {order: 2;}
+        > .icon {
+            order: 1;
+            margin-right: .1em;
         }
-        .load{
+        &.icon-right{
+            > .g-button-content{ order: 1;}
+            > .icon{ order: 2; margin-right: 0; margin-left: .1em}
+        }
+
+        .load {
             animation: spin 1.5s infinite linear;
         }
     }

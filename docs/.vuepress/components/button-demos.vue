b43d588f6b09836b4006a3b46c0c46da4b5ed746
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-16 17:58:15 +0800

    完成button文档

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,28 +1,51 @@
 <template>
     <div>
-        <g-button>默认按钮</g-button>
-        <g-button :load="true">默认按钮</g-button>
-        <g-button icon="error">默认按钮</g-button>
-        <g-button disabled>默认按钮</g-button>
+        <p><strong>预览</strong></p>
+        <div>
+            <g-button>默认按钮</g-button>
+            <g-button :load="true">默认按钮</g-button>
+            <g-button icon="error">默认按钮</g-button>
+            <g-button disabled>默认按钮</g-button>
+        </div>
+        <p><strong>代码</strong></p>
+        <pre><code>{{buttonContent}}</code></pre>
 
-        <pre><code>{{content}}</code></pre>
+        <h3>按钮组合</h3>
+        <p><strong>预览</strong></p>
+        <div style="padding-top: .5em;">
+            <g-button-group>
+                <g-button icon="left">上一页</g-button>
+                <g-button icon="right" icon-position="right">下一页</g-button>
+            </g-button-group>
+        </div>
+        <p><strong>代码</strong></p>
+        <pre><code>{{buttonGroupContent}}</code></pre>
+
+
     </div>
 
 </template>
 <script>
     import Button from '../../../src/button'
-
+    import ButtonGroup from '../../../src/button-group'
     export default {
         components: {
-            'g-button': Button
+            'g-button': Button,
+            'g-button-group': ButtonGroup
         },
         data() {
             return {
-                content: `
+                buttonContent: `
         <g-button>默认按钮</g-button>
         <g-button :load="true">默认按钮</g-button>
         <g-button icon="error">默认按钮</g-button>
         <g-button disabled>默认按钮</g-button>
+                `.replace(/\t+| +/g, '').trim(),
+                buttonGroupContent:`
+                <g-button-group>
+                        <g-button icon="left">上一页</g-button>
+                        <g-button icon="right" icon-position="right">下一页</g-button>
+                   </g-button-group>
                 `.replace(/\t+| +/g, '').trim()
             }
         }
--- docs/components/button.md
+++ docs/components/button.md
@@ -2,9 +2,14 @@
 title: Button
 ---
 
-# Button
+# 按钮
 
-使用方法：
+
+
+
+<h3 style="padding-bottom: 1em">简单用法</h3>
 <ClientOnly>
     <button-demos></button-demos>
 </ClientOnly>
+
+> 可选的icon：setting, error, load, info, left, right, download, thumbs-up
